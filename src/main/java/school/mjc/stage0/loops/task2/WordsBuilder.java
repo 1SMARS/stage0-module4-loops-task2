3a888d900eef1248ffeb4ebd012ed6b25c9506b6
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-11-05 14:04:07 +0600

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,12 +2,11 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
-        int count = 0;
-        StringBuilder phrase = new StringBuilder();
-        while (count < chars.length) {
-            phrase.append(chars[count++]);
+        int i=0;
+        while (i< chars.length){
+            System.out.print(chars[i]);
+            i++;
         }
-        System.out.print(phrase);
 
     }
 }
