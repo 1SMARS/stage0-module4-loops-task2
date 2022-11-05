0be4c487bb6ce99312ad8b955b725566c79d18ea
Rustam Isma <71608867+1SMARS@users.noreply.github.com>
2022-11-05 14:11:25 +0600

    Update PowerOfTwo.java

--- src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
+++ src/main/java/school/mjc/stage0/loops/task2/PowerOfTwo.java
@@ -2,23 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class PowerOfTwo {
     public void printPower(int power) {
-        while (power > 0) {
-
-            if (power == 1) {
-
-                System.out.println(power);
-            }
-
-            if (power % 2 != 0) {
-
-                System.out.println("too much power");
-            }
-            else {
-
-                power = power / 2;
+        if(power<0)
+            System.out.println("too much power");
+        else {
+            int start = 0;
+            while (start <= power){
+                System.out.println((int)Math.pow(2,start));
+                start++;
             }
         }
-
-        System.out.println("too much power");
     }
 }
